506ff49365722aed5d2d8c1f5794dd08ca321e34
stefant234 <stefant1996@hotmail.de>
2015-06-28 14:45:37 +0200

    Initial Commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#1 SMP PREEMPT Thu Jun 25 18:08:21 KST 2015"
+#define LINUX_COMPILE_BY "oms"
+#define LINUX_COMPILE_HOST "oms-Samsung-Desktop-System"
+#define LINUX_COMPILER "gcc version 4.8 (GCC) "
